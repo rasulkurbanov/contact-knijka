f21337acc884a75ed32505699c9326ca12c926d9
rasulkurbanov <rasuljohn139@gmail.com>
2021-03-18 16:13:44 +0500

    Add jwt, sending token

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,68 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+
 
 
 //@route POST api/auth
 //@desc  Register a user
 //@access Public
-router.post('/', (req, res) => {
-  res.send('Register a user')
-})
+router.post('/',
+  check('email').isEmail(),
+  check('password').exists(),
+
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+
+    try {
+      let user = await User.findOne({ email })
+
+      if (!user) return res.status(400).send('Invalid credentials')
+
+
+      const isMatch = await bcrypt.compare(password, user.password)
+
+      if(isMatch === false) return res.status(400).send('Invalid password or email')
+
+      //Creating payload
+      const payload = {
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name
+        }
+      }
+
+      //Implementing jsonwebtoken
+      jwt.sign(
+        payload,
+        config.get('JWT_SECRET'),
+        { expiresIn: config.get('TOKEN_EXPIRE') },
+        function (err, token) {
+          if (err) throw new Error()
+          res.send({ token: token })
+        }
+      )
+
+
+    }
+    catch (err) {
+      console.log(err)
+      res.status(500).json({ error: err.message })
+    }
+
+
+  })
 
 
 //@route GET api/auth
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,9 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcryptjs')
 
 
 //@route POST api/users
@@ -10,27 +13,75 @@ const { check, validationResult } = require('express-validator');
 router.post('/',
   //Initializing express-validator to validate errors
   check('name')
+    .not()
     .isEmpty()
     .isLength({ min: 5 })
     .withMessage('Name must be at least 5 characters long'),
   check('email')
+    .not()
     .isEmpty()
     .isLength({ min: 5 })
     .withMessage('Email must be at least 5 characters long')
     .matches(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)
     .withMessage('Please enter a valid email address!'),
   check('password')
+    .not()
     .isEmpty()
     .isLength({ min: 10 })
-    .withMessage('Password must contain at least 10 characters')
-    .matches(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
-    .withMessage('Password must contain at least one special character'),
-  (req, res) => {
+    .withMessage('Password must contain at least 10 characters'),
+
+  async (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() })
     }
-    res.send(req.body)
+
+    const { name, email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+
+      if(user) {
+        return res.status(404).send(`User with this email or name is already exists`)
+      }
+
+      //Initializing user to save database
+      user = new User({
+        name, 
+        email,
+        password
+      })
+      
+      //Generating salt using bcrypt
+      const salt = await bcrypt.genSalt(10)
+      user.password = await bcrypt.hash(user.password, salt)
+
+      await user.save()
+
+      //Creating payload
+      const payload = {
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name
+        }
+      }
+
+      //Implementing jsonwebtoken
+      jwt.sign(
+        payload, 
+        config.get('JWT_SECRET'), 
+        {expiresIn: config.get('TOKEN_EXPIRE')},
+        function(err, token) {
+          if(err) throw new Error()
+          res.send({ token: token })
+        }
+        )
+    }
+    catch(err) {
+      console.log(err)
+      res.status(500).send(err)
+    }
   })
   
 
